347b8eb179f52d39dcabeeb94d0453f200b22ede
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-26 20:27:20 +1100

    performance improved by still fail

--- src/com/codewardev/MinPathSquare.java
+++ src/com/codewardev/MinPathSquare.java
@@ -1,66 +1,70 @@
 package com.codewardev;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Stack;
 
 // https://www.codewars.com/kata/5845e3f680a8cf0bad00017d/train/java
 
 public class MinPathSquare {
 
 	private static int minPath;
-	private static Stack<Integer> gridStack;
-	private static Map<Integer, Stack<Integer>> allStacks;
+	private static int distanceFromBeginning;
 	
 	public static int minPath(int[][] grid, int x, int y) {
 		minPath = -1;
-		gridStack = new Stack<Integer>();
-		//allStacks = new HashMap<Integer, Stack<Integer>>();
-		GridNode gNode = constructGridNode(grid, 0, 0);
-		traverseGridNode(gNode, y, x);
+		distanceFromBeginning = 0;
+		int[][] newGrid = new int[y+1][x+1];
+		newGrid = formNewGrid(grid, x, y);
+		//printGrid(newGrid);
+		//System.out.println("Start construct");
+		//long current = System.currentTimeMillis();
+		GridNode gNode = constructGridNode(newGrid, 0, 0, x, y);
+		//System.out.println("end construct in "+(System.currentTimeMillis()-current)+" milliseconds");
 		System.out.println("MinPath is: "+minPath);
-		//printStack();
 		return minPath;
 	}
 
-	private static void printStack() {
-		Stack<Integer> minStack = allStacks.get(minPath);
-		Iterator<Integer> iter = minStack.iterator();
-		while(iter.hasNext()) {
-			System.out.println("Integer: "+iter.next());
-		}
-		
-	}
 
-	private static void traverseGridNode(GridNode gNode, int x, int y) {
-		gridStack.add(gNode.getValue());
-		if(gNode.getKey().equalsIgnoreCase(x+""+y)) {
-			int totalPath = gridStack.stream().mapToInt(Integer::valueOf).sum();
-			if(minPath == -1 || minPath > totalPath) {
-				//allStacks.put(totalPath, (Stack<Integer>) gridStack.clone());
-				minPath = totalPath;
+	private static int[][] formNewGrid(int[][] grid, int x, int y) {
+		int[][] newGrid = new int[y+1][x+1];
+		for(int i=0; i<=y; i++) {
+			for(int j=0; j<=x; j++) {
+				newGrid[i][j] = grid[i][j];
 			}
-			gridStack.pop();
-			return;
 		}
-		if(gNode.getRight() != null) traverseGridNode(gNode.getRight(), x, y);
-		if(gNode.getDown() != null) traverseGridNode(gNode.getDown(), x, y);
-		gridStack.pop();
+		return newGrid;
 	}
 
-	private static GridNode constructGridNode(int[][] grid, int i, int j) {
+
+	private static void printGrid(int[][] grid) {
+		System.out.println("Grid:");
+		for(int i=0; i<grid.length; i++) {
+			for(int j=0; j<grid[i].length; j++) {
+				System.out.print(grid[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+
+	private static GridNode constructGridNode(int[][] grid, int i, int j, int x, int y) {
 		GridNode aNode = new GridNode(grid[i][j], i+""+j);
+		distanceFromBeginning += aNode.getValue();
+		aNode.setDistance(distanceFromBeginning);
+		if(i == y && j == x) {
+			if(minPath == -1 || minPath > aNode.getDistance()) {
+				minPath = aNode.getDistance();
+			}
+		}
 		int rIndex = j+1;
 		int dIndex = i+1;
 		if(rIndex < grid[i].length) {
-			aNode.setRight(constructGridNode(grid, i, rIndex));
+			aNode.setRight(constructGridNode(grid, i, rIndex, x, y));
 		}
 		
 		if(dIndex < grid.length) {
-			aNode.setDown(constructGridNode(grid, dIndex, j));
+			aNode.setDown(constructGridNode(grid, dIndex, j, x, y));
 		}
 		
+		distanceFromBeginning -= aNode.getValue();
 		return aNode;
 	}
 
@@ -70,6 +74,7 @@ public class MinPathSquare {
 class GridNode {
 	private int value;
 	private String key;
+	private int distance;
 	private GridNode right;
 	private GridNode down;
 	
@@ -101,6 +106,14 @@ class GridNode {
 	}
 	public void setDown(GridNode down) {
 		this.down = down;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
 	}
 	
 	
