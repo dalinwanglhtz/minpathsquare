6d963444e30a231a42d2ca73ef67087cc287b7b3
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-27 22:35:41 +1100

    changing algorithm

--- src/com/codewardev/MinPathSquare.java
+++ src/com/codewardev/MinPathSquare.java
@@ -7,25 +7,56 @@ public class MinPathSquare {
 
 	private static int minPath;
 	private static int distanceFromBeginning;
+	private static int shortestI;
+	private static int shortestJ;
+	private static int partialMinPath;
+	private static int finalMinPath;
 	
 	public static int minPath(int[][] grid, int x, int y) {
 		minPath = -1;
 		distanceFromBeginning = 0;
-		int[][] newGrid = formNewGrid(grid, x, y);
+		partialMinPath = -1;
+		finalMinPath = 0;
+		int[][] newGrid = formNewGrid(grid, 0, 0, x, y);
 		printGrid(newGrid);
-		//System.out.println("Start construct");
-		//long current = System.currentTimeMillis();
-		constructGridNode(newGrid, 0, 0, x, y);
-		//System.out.println("end construct in "+(System.currentTimeMillis()-current)+" milliseconds");
+		System.out.println("Start construct");
+		long current = System.currentTimeMillis();
+		findPartialMinPath(newGrid, 0, 0);
+		finalMinPath += partialMinPath;
+		partialMinPath = -1;
+		constructGridNode(newGrid, shortestI, shortestJ, x, y);
 		System.out.println("MinPath is: "+minPath);
+		finalMinPath += minPath;
+		
+		System.out.println("end construct in "+(System.currentTimeMillis()-current)+" milliseconds");
+		System.out.println("finalMinPath is: "+finalMinPath);
 		return minPath;
 	}
 
 
-	private static int[][] formNewGrid(int[][] grid, int x, int y) {
+	private static void findPartialMinPath(int[][] newGrid, int x, int y) {
+		int j = newGrid.length-1;
+		for(int i=0; i<newGrid.length; i++) {
+			constructGridNode(newGrid, x, y, i, j);
+			System.out.print("Minpath: "+minPath+" i:"+i+" j:"+j);
+			if(partialMinPath == -1 || partialMinPath > minPath) {
+				partialMinPath = minPath;
+				shortestI = j;
+				shortestJ = i;
+			}
+			System.out.println(" with value: "+newGrid[i][j]);
+			j--;
+			minPath = -1;
+		}
+		partialMinPath -= newGrid[shortestI][shortestJ];
+		System.out.println("ShortestI: "+shortestI+" ShortestJ: "+shortestJ+" finalMin: "+partialMinPath);
+	}
+
+
+	private static int[][] formNewGrid(int[][] grid, int startX, int startY, int x, int y) {
 		int[][] newGrid = new int[y+1][x+1];
-		for(int i=0; i<=y; i++) {
-			for(int j=0; j<=x; j++) {
+		for(int i=startY; i<=y; i++) {
+			for(int j=startX; j<=x; j++) {
 				newGrid[i][j] = grid[i][j];
 			}
 		}
