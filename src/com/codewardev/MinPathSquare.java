3e380edef561c1bfa64e5136ee3a99e5b93184a7
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-29 00:12:35 +1100

    refined algorithm

--- src/com/codewardev/MinPathSquare.java
+++ src/com/codewardev/MinPathSquare.java
@@ -1,5 +1,8 @@
 package com.codewardev;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 // https://www.codewars.com/kata/5845e3f680a8cf0bad00017d/train/java
 
@@ -7,86 +10,40 @@ public class MinPathSquare {
 
 	private static int minPath;
 	private static int distanceFromBeginning;
-	private static int shortestI;
-	private static int shortestJ;
-	private static int partialMinPath;
-	private static int finalMinPath;
+	private static Map<String, Integer> shortestPaths = new HashMap<String, Integer>();
 	
-	public static int minPath(int[][] grid, int x, int y) {
+	public static int minPath(int[][] grid, int y, int x) {
 		minPath = -1;
 		distanceFromBeginning = 0;
-		shortestI = 0;
-		shortestJ = 0;
-		partialMinPath = -1;
-		finalMinPath = 0;
 		int[][] newGrid = formNewGrid(grid, 0, 0, x, y);
-		printGrid(newGrid);
-		System.out.println("Start construct");
-		long current = System.currentTimeMillis();
 		
-		while(shortestI != y && shortestJ != x) {
-			findPartialMinPath(newGrid, shortestJ, shortestI);
-			finalMinPath += partialMinPath;
-			partialMinPath = -1;
-			newGrid = formNewGrid(newGrid, shortestI, shortestJ, x, y);
+		if(!shortestPaths.containsKey(x+""+y)) {
 			printGrid(newGrid);
-		}
-		
-		
-//		constructGridNode(newGrid, shortestJ, shortestI, x, y);
-//		System.out.println("MinPath is: "+minPath);
-//		finalMinPath += minPath;
-		
-		System.out.println("end construct in "+(System.currentTimeMillis()-current)+" milliseconds");
-		System.out.println("finalMinPath is: "+finalMinPath);
-		return minPath;
-	}
-
-
-	private static void findPartialMinPath(int[][] newGrid, int x, int y) {
-		int j = newGrid.length-1;
-		for(int i=x; i<newGrid.length; i++) {
-			constructGridNode(newGrid, x, y, i, j);
-			System.out.print("Minpath: "+minPath+" i:"+i+" j:"+j);
-			if(partialMinPath == -1 || partialMinPath > minPath) {
-				partialMinPath = minPath;
-				shortestI = i;
-				shortestJ = j;
-			}
-			System.out.println(" with value: "+newGrid[i][j]);
-			j--;
-			minPath = -1;
-		}
-		partialMinPath -= newGrid[shortestJ][shortestI];
-		System.out.println("ShortestI: "+shortestI+" ShortestJ: "+shortestJ+" finalMin: "+partialMinPath);
-	}
-
-
-	private static int[][] formNewGrid(int[][] grid, int startX, int startY, int x, int y) {
-		int[][] newGrid = new int[y+1][x+1];
-		for(int i=startY; i<=y; i++) {
-			for(int j=startX; j<=x; j++) {
-				newGrid[i][j] = grid[i][j];
+			for(int i=0; i<newGrid.length; i++) {
+				for(int j=0; j<newGrid[i].length; j++) {
+					if(!shortestPaths.containsKey(i+""+j)) {
+						constructGridNode(newGrid, 0, 0, i, j);
+						shortestPaths.put(i+""+j, minPath);
+						//System.out.println("Key: "+i+""+j+" Value: "+minPath);
+						minPath = -1;
+						distanceFromBeginning = 0;
+					}
+				}
 			}
 		}
-		return newGrid;
-	}
-
-
-	private static void printGrid(int[][] grid) {
-		System.out.println("Grid:");
-		for(int i=0; i<grid.length; i++) {
-			for(int j=0; j<grid[i].length; j++) {
-				System.out.print(grid[i][j]+" ");
-			}
-			System.out.println();
-		}
+		
+//		for(Entry<String, Integer> set: shortestPaths.entrySet()) {
+//			System.out.println("Key: "+set.getKey()+" minPath: "+set.getValue());
+//		}
+		
+		System.out.println("finalMinPath is: "+shortestPaths.get(x+""+y));
+		return shortestPaths.get(x+""+y);
 	}
 
 
 	private static void constructGridNode(int[][] grid, int i, int j, int x, int y) {
 		distanceFromBeginning += grid[i][j];
-		if(i == y && j == x) {
+		if(i == x && j == y) {
 			if(minPath == -1 || minPath > distanceFromBeginning) {
 				minPath = distanceFromBeginning;
 			}
@@ -104,6 +61,28 @@ public class MinPathSquare {
 		}
 		
 		distanceFromBeginning -= grid[i][j];
+	}
+	
+
+	private static int[][] formNewGrid(int[][] grid, int startX, int startY, int x, int y) {
+		int[][] newGrid = new int[x+1][y+1];
+		for(int i=startX; i<=x; i++) {
+			for(int j=startY; j<=y; j++) {
+				newGrid[i][j] = grid[i][j];
+			}
+		}
+		return newGrid;
+	}
+
+
+	private static void printGrid(int[][] grid) {
+		System.out.println("Grid:");
+		for(int i=0; i<grid.length; i++) {
+			for(int j=0; j<grid[i].length; j++) {
+				System.out.print(grid[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
--- tests/com/codewardev/test/SolutionTest.java
+++ tests/com/codewardev/test/SolutionTest.java
@@ -13,19 +13,19 @@ class SolutionTest {
                 { 1, 2, 3, 6, 2, 8, 1 },
                 { 4, 8, 2, 4, 3, 1, 9 },
                 { 1, 5, 3, 7, 9, 3, 1 },
-                { 4, 9, 2, 8, 6, 9, 5 },
-                { 7, 6, 1, 4, 7, 2, 6 },
+                { 4, 9, 2, 1, 6, 9, 5 },
+                { 7, 6, 8, 4, 7, 2, 6 },
                 { 2, 1, 6, 2, 4, 8, 7 },
                 { 8, 4, 3, 9, 2, 5, 8 }
             };
             
 	@Test
 	public void smallTests() {
-	//	assertEquals(1, MinPathSquare.minPath(smallSquare, 0, 0));
-	//	assertEquals(5, MinPathSquare.minPath(smallSquare, 0, 1));
-	//	assertEquals(11, MinPathSquare.minPath(smallSquare, 2, 2));
-	//	assertEquals(24, MinPathSquare.minPath(smallSquare, 4, 2));
+		assertEquals(1, MinPathSquare.minPath(smallSquare, 0, 0));
+		assertEquals(5, MinPathSquare.minPath(smallSquare, 0, 1));
+		assertEquals(11, MinPathSquare.minPath(smallSquare, 2, 2));
+		assertEquals(24, MinPathSquare.minPath(smallSquare, 4, 2));
 		assertEquals(39, MinPathSquare.minPath(smallSquare, 6, 6));
-	//	assertEquals(24, MinPathSquare.minPath(smallSquare, 4, 5));
+		assertEquals(24, MinPathSquare.minPath(smallSquare, 4, 5));
 	}
 }
